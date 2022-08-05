f94e8565f3e27a4c4d4efbbbbb44af4027061ba5
jipwark <79764472+jipwark@users.noreply.github.com>
2022-08-04 22:30:44 -0700

    Final commit

--- MindfulMagic/ViewController.swift
+++ MindfulMagic/ViewController.swift
@@ -8,42 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var Word1: UITextField!
-    
-    @IBOutlet weak var Mood1: UITextField!
-    
-    @IBOutlet weak var Happy1: UITextField!
-    
-    @IBOutlet weak var Good1: UITextField!
-    
-    @IBOutlet weak var Improve1: UITextField!
-    
-    let words = CoreDataFile()
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func DoneButton(_ sender: Any) {
-        if let Word = Word1.text {
-            print(words.Words)
-        }
-        if let Mood = Mood1.text {
-            print(words.Moods)
-        }
-        if let Happy = Happy1.text {
-            print(words.Happies)
-        }
-        if let Good = Good1.text {
-            print(words.Goods)
-        }
-        if let Improvement = Improve1.text {
-            print(words.Improvements)
-        }
+    @IBAction func doneButton(_ sender: Any) {
+ 
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if let addVC = segue.destination as? AnswersViewController {
